a64dd48a453f4ca141cbdd8f8777f31885e417a5
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-07-23 23:58:26 +0900

    Database table SQL updated

--- Database_Table_Creation.sql
+++ Database_Table_Creation.sql
@@ -22,35 +22,51 @@ CREATE TABLE Professors (
     Dept VARCHAR(30) NOT NULL,
     CONSTRAINT PK_Professors PRIMARY KEY(ID)
 );
+ALTER TABLE Professors AUTO_INCREMENT = 1;
+INSERT INTO Professors(First_Name, Last_Name, Email, Dept) VALUES
+	("Big", "Brain", "bigbrain@temple.edu", "Computer Science");
 CREATE TABLE Courses (
-	ID INTEGER NOT NULL,
+	ID INTEGER NOT NULL AUTO_INCREMENT,
     Title VARCHAR(30) NOT NULL,
     Credits INTEGER NOT NULL,
     Department VARCHAR(30) NOT NULL,
     CONSTRAINT PK_Courses PRIMARY KEY(ID)
 );
+ALTER TABLE Courses AUTO_INCREMENT = 1;
+INSERT INTO Courses(Title, Credits, Department) VALUES
+	("Database Management", 4, "Computer Science");
 CREATE TABLE Semester (
-	Course_ID INTEGER NOT NULL,
+	Semester_ID INTEGER NOT NULL AUTO_INCREMENT,
     Semester VARCHAR(30) NOT NULL,
-    Year INTEGER,
-    CONSTRAINT PK_Semester PRIMARY KEY(Course_ID, Semester, Year),
-    CONSTRAINT FK_Semester_Courses FOREIGN KEY(Course_ID) REFERENCES Courses(ID)
+    Offered_Year INTEGER,
+    CONSTRAINT PK_Semester PRIMARY KEY(Semester_ID)
 );
+ALTER TABLE Semester AUTO_INCREMENT = 1;
+INSERT INTO Semester(Semester, Offered_Year) VALUES
+	("Spring", "2023");
+
 CREATE TABLE Class (
-	Class_ID INTEGER NOT NULL,
-    Course_ID INTEGER NOT NULL,
-    Semester VARCHAR(30) NOT NULL,
+	Class_ID INTEGER NOT NULL AUTO_INCREMENT,
+    Semester_ID INTEGER NOT NULL,
+	Course_ID INTEGER NOT NULL,
     Professor_ID INTEGER NOT NULL,
-    TimeDay DATETIME,
-    CONSTRAINT PK_Class PRIMARY KEY(Class_ID, Course_ID, Semester),
-    CONSTRAINT FK_Class_Course FOREIGN KEY(Class_ID) REFERENCES Courses(ID)
+    Days VARCHAR(10) NOT NULL,
+    StartTime TIME NOT NULL,
+    EndTime TIME NOT NULL,
+    CONSTRAINT PK_Class PRIMARY KEY(Class_ID),
+    CONSTRAINT FK_Class_Course FOREIGN KEY(Course_ID) REFERENCES Courses(ID),
+    CONSTRAINT FK_Class_Semester_ID FOREIGN KEY(Semester_ID) REFERENCES Semester(Semester_ID),
+    CONSTRAINT FK_Class_Professor_ID FOREIGN KEY(Professor_ID) REFERENCES Professors(ID)
 );
+ALTER TABLE Class AUTO_INCREMENT = 1;
+INSERT INTO Class(Semester_ID, Course_ID, Professor_ID, Days, StartTime, EndTime) VALUES
+	(1, 1, 1, "MWF", "10:00", "11:00");
 CREATE TABLE Attendance (
 	Student_ID INTEGER NOT NULL,
     Class_ID INTEGER NOT NULL,
     Present BOOLEAN NOT NULL,
-    Date TIMESTAMP NOT NULL,
-    CONSTRAINT PK_Attendance PRIMARY KEY(Student_ID, Class_ID, Date),
+    DateOf TIMESTAMP NOT NULL,
+    CONSTRAINT PK_Attendance PRIMARY KEY(Student_ID, Class_ID, DateOf),
     -- CONSTRAINT FK_Attendance_Students FOREIGN KEY(Student_ID) REFERENCES Students(Student_ID),
     CONSTRAINT FK_Attendance_Class FOREIGN KEY(Class_ID) REFERENCES Class(Class_ID)
 );
