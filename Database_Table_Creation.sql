0a4be8236eb1acead45c651a9696a744ad5d6a74
Juan Kenichi Sutan <kenichi.jks@gmail.com>
2023-07-24 14:35:45 +0900

    Update Database_Table_Creation.sql

--- Database_Table_Creation.sql
+++ Database_Table_Creation.sql
@@ -1,30 +1,19 @@
-DROP TABLE IF EXISTS Students, Professors, Courses, Semester, Class, Attendance;
+DROP TABLE IF EXISTS Students, Courses, Semester, Class, Attendance;
 CREATE TABLE Students (
-	ID INTEGER NOT NULL AUTO_INCREMENT,
+	Student_ID INTEGER NOT NULL AUTO_INCREMENT,
     First_Name VARCHAR(30),
     Last_Name VARCHAR(30),
     Gender CHAR(1),
     Email VARCHAR(30) NOT NULL,
     Address VARCHAR(80) NOT NULL,
     Credits INTEGER,
-    Status VARCHAR(20),
+    Current_Status VARCHAR(20),
     Major VARCHAR(20),
     CONSTRAINT PK_Students PRIMARY KEY(ID)
 );
 ALTER TABLE Students AUTO_INCREMENT = 910000001;
-INSERT INTO Students(First_Name, Last_Name, Gender, Email, Address, Credits, Status, Major) VALUES
+INSERT INTO Students(First_Name, Last_Name, Gender, Email, Address, Credits, Current_Status, Major) VALUES
 	("Arvindh", "Velrajan", 'M', "tun67213@temple.edu", "random", 12, "Part Time", "Computer Science");
-CREATE TABLE Professors (
-	ID INTEGER NOT NULL AUTO_INCREMENT,
-    First_Name VARCHAR(30),
-    Last_Name VARCHAR(30),
-    Email VARCHAR(30) NOT NULL,
-    Dept VARCHAR(30) NOT NULL,
-    CONSTRAINT PK_Professors PRIMARY KEY(ID)
-);
-ALTER TABLE Professors AUTO_INCREMENT = 1;
-INSERT INTO Professors(First_Name, Last_Name, Email, Dept) VALUES
-	("Big", "Brain", "bigbrain@temple.edu", "Computer Science");
 CREATE TABLE Courses (
 	ID INTEGER NOT NULL AUTO_INCREMENT,
     Title VARCHAR(30) NOT NULL,
@@ -39,7 +28,8 @@ CREATE TABLE Semester (
 	Semester_ID INTEGER NOT NULL AUTO_INCREMENT,
     Semester VARCHAR(30) NOT NULL,
     Offered_Year INTEGER,
-    CONSTRAINT PK_Semester PRIMARY KEY(Semester_ID)
+    CONSTRAINT PK_Semester PRIMARY KEY(Semester_ID),
+    CONSTRAINT Natural_PK_Semester UNIQUE (Semester, Offered_Year)
 );
 ALTER TABLE Semester AUTO_INCREMENT = 1;
 INSERT INTO Semester(Semester, Offered_Year) VALUES
@@ -62,11 +52,13 @@ ALTER TABLE Class AUTO_INCREMENT = 1;
 INSERT INTO Class(Semester_ID, Course_ID, Professor_ID, Days, StartTime, EndTime) VALUES
 	(1, 1, 1, "MWF", "10:00", "11:00");
 CREATE TABLE Attendance (
+	Attendannce_ID INTEGER NOT NULL,
 	Student_ID INTEGER NOT NULL,
     Class_ID INTEGER NOT NULL,
     Present BOOLEAN NOT NULL,
-    DateOf TIMESTAMP NOT NULL,
+    DateOf DATE NOT NULL,
     CONSTRAINT PK_Attendance PRIMARY KEY(Student_ID, Class_ID, DateOf),
-    -- CONSTRAINT FK_Attendance_Students FOREIGN KEY(Student_ID) REFERENCES Students(Student_ID),
+    CONSTRAINT Natural_PK_Attendance UNIQUE (Student_ID, Class_ID, Attendance_Date),
+    CONSTRAINT FK_Attendance_Students FOREIGN KEY(Student_ID) REFERENCES Students(Student_ID),
     CONSTRAINT FK_Attendance_Class FOREIGN KEY(Class_ID) REFERENCES Class(Class_ID)
 );
